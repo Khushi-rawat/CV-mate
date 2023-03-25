a05568fe0816d92194a18ab7bf1964206fe2e1ac
foram2123 <barotforam005@gmail.com>
2023-03-25 23:20:26 +0530

    new pages

---
+++ f_s_l.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+    <head>
+
+        <title>student information</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body>
+
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Enrollment No.</th>
+      <th scope="col">Name</th>
+      <th scope="col">Go To</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <td>200170116014</td>
+      <td>Foram</td>
+      <td><a href="200170116014.cv">View Profile</a></td>
+    </tr>
+    <tr>      
+      <td>830546578464</td>
+      <td>Xyz</td>
+      <td><a href="830546578464.cv">View Profile</a></td>
+    </tr>
+    </tbody>
+    
+</table>
+
+</body>
+</html>
