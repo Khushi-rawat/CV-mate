87e4801c64d2ade60e533d0f96aa6cd4932eb3fd
Khushi Rawat <khushi050402@gmail.com>
2023-03-26 12:58:26 +0530

    Faculty pages

---
+++ faculty_requests.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Student Request</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</head>
+<body class="m-5">
+    <table class="table table-bordered">
+    <thead>
+    <tr>
+      <th scope="col">Enrollment No.</th>
+      <th scope="col">Name</th>
+      <th scope="col">View Request</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>12345678909</td>
+      <td>wertyu</td>
+      <td><a href="">View</a></td>
+    </tr>
+    <tr>
+      <td>09876543212</td>
+      <td>sdbgju</td>
+      <td><a href="">View</a></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
---
+++ faculty_viewrequest.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Student Request</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</head>
+<body class="m-5">
+    <div class="card">
+    <div class="card-body">
+    <h5 class="card-title">Project/Internship/Others</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Title/Name</h6>
+    <p class="card-text">DESCRIBTION: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Project Link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+  <img src="" class="card-img-bottom" alt="FILE ">
+</div>
+  <div class="d-grid gap-2 d-md-block m-3">
+  <button class="btn btn-success me-md-5" type="button" value="approve">Approve</button>
+  <button class="btn btn-danger" type="button" value="reject">Reject</button>
+</div>
+</body>
+</html>
