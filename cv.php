619cc4a8e7c4c71951dfa44ceb138c08e26aa26c
Hardi Jayani <hardijayani29@gmail.com>
2023-03-29 15:17:05 +0530

    new pages

--- cv.php
+++ cv.php
@@ -4,9 +4,157 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NAME</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>CV</title>
+
 </head>
 <body>
-    weruitfhi
+
+<section class="vh-120" style="background-color: #003975;">
+<div class="row d-flex justify-content-center align-items-center h-100">
+<div class="col-lg-10">
+<h5 class="mb-3" style="color: white;">Student Details</h5>
+
+        <div class="card mb-3">
+          <div class="card-body">
+          
+            <div class="row">
+              <div class="col-sm-2">
+                <p class="mb-0">Full Name</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">Johnatan Smith</p>
+              </div>
+              <div class="col-sm-2">
+                <p class="mb-0">Enrollment</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">245786434232</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-2">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">example@example.com</p>
+              </div>
+              <div class="col-sm-2">
+                <p class="mb-0">Mobile</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">(098) 765-4321</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-2">
+                <p class="mb-0">Branch</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">Information Technology</p>
+              </div>
+              <div class="col-sm-2">
+                <p class="mb-0">Gender</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">Male</p>
+              </div>
+            </div>
+            
+            <hr>
+            <div class="row">
+              <div class="col-sm-2">
+                <p class="mb-0">Passout Year</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">2024</p>
+              </div>
+              <div class="col-sm-2">
+                <p class="mb-0">Current CGPA</p>
+              </div>
+              <div class="col-sm-4">
+                <p class="text-muted mb-0">8.72</p>
+              </div>
+</div>
+          </div>
+</div>
+</div>
+        
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+
+      <h5 class="mb-3" style="color: white;">Your Activities</h5>
+        <div class="card" style="border-radius: 15px;">
+          <div class="card-body p-5">
+          
+
+            <ul class="list-group mb-0">
+            <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                <div class="d-flex align-items-center">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam obcaecati deleniti vel, atque ut ad commodi possimus, modi rem, provident ipsum excepturi molestias omnis! Eveniet non laboriosam numquam dolorem.
+                </div>
+                
+              </li>
+              <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                <div class="d-flex align-items-center">
+                  
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, omnis. Ab saepe, sunt tenetur culpa architecto, animi esse temporibus accusantium numquam, veritatis laudantium explicabo unde deleniti quos quasi nulla facere!
+                </div>
+                
+              </li>
+              <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                <div class="d-flex align-items-center">
+                  
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos obcaecati aliquid velit voluptatibus nam laudantium quis aut, iure, ducimus corporis rem explicabo ut fugiat. Quae quod dolorem quis incidunt doloremque!
+                </div>
+                
+              </li>
+              <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                <div class="d-flex align-items-center">
+                  
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nemo repudiandae fugiat at, quas soluta aperiam velit cupiditate perspiciatis animi excepturi praesentium tempore maiores nulla provident enim fugit veniam temporibus.
+                </div>
+                
+              </li>
+              <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-0">
+                <div class="d-flex align-items-center">
+                  
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus totam illo iste quod, dolorem exercitationem autem delectus corporis modi nesciunt ut quasi voluptas, voluptate iusto velit corrupti eos repudiandae et?
+                </div>
+                
+              </li>
+              <h6>Activity 1</h6>
+              <li
+                class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+                <div class="d-flex align-items-center">
+                  
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea perspiciatis reprehenderit consequatur harum. Iure non quam reiciendis, a, quasi natus molestiae perferendis praesentium incidunt ut maxime eius provident labore nulla.
+                </div>
+               
+              </li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
 </body>
 </html>
