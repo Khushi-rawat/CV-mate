46d94c94232328a72ffd911e78f4e6afc66914c5
foram2123 <barotforam005@gmail.com>
2023-03-29 23:33:05 +0530

    hi

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-<<<<<<< HEAD
-
+$server="localhost";
+$name="root";
+$db="cv";
+$password="";
 $conn=mysqli_connect("localhost","root","","cv");
 if(!$conn){
     die("Connection failed:".mysqli_connect_error());
 }
-=======
-$conn = mysqli_connect("localhost","root","","cv");
->>>>>>> 31b393f70638c1ef506c1f8ab06515a9a234aedd
 ?>
