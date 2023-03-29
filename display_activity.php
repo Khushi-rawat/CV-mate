4c7681bff54feb0b3336223cc7d7e6b04cc95a7a
foram2123 <barotforam005@gmail.com>
2023-03-29 21:06:12 +0530

    bhgtfhg

---
+++ display_activity.php
@@ -0,0 +1,40 @@
+<!doctype html>
+
+    <head>
+
+        <title>Activity List</title>
+       
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body class="m-5">
+    
+    <table class="table table-bordered" id="myTable">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Main Activity</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Attachment</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Status</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+
+  <tbody id="xyz">
+    <tr>
+      <td>Project</td>
+      <td>Cv mate</td>
+      <td>qpwrjeefsdfj sifjsddfhdvnxc sndskfnsdlkfxdkl fkjsdfsdlkbdsbxdbdbs qpwrjeefsdfj sifjsddfhdvnxc sndskfnsdlkfxdkl fkjsdfsdlkbdsbxdbdbs qpwrjeefsdfj sifjsddfhdvnxc sndskfnsdlkfxdkl fkjsdfsdlkbdsbxdbdbs</td>
+      <td>abc</td>
+      <td>xyz</td>
+      <td>pqr</td>
+      <td>pqr</td>
+    </tr>
+    
+    </tbody>
+    
+</table>
--- student_li.php
+++ student_li.php
@@ -3,15 +3,23 @@
     <head>
 
         <title>student information</title>
-
+       
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
+        <style>
+          #myInput
+          {
+            margin-bottom: 7px;
+            width: 100%;
+          }
+        </style>
     </head>
 
     <body class="m-5">
+    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for enrollment number" title="Type in a name">
 
-    <table class="table table-bordered">
+    <table class="table table-bordered" id="myTable">
   <thead class="thead-dark">
     <tr>
       <th scope="col">Enrollment No.</th>
@@ -20,7 +28,7 @@
     </tr>
   </thead>
 
-  <tbody>
+  <tbody id="xyz">
     <tr>
       <td>200170116014</td>
       <td>Foram</td>
@@ -31,9 +39,81 @@
       <td>Khushi</td>
       <td><a href="cv.php">View</a></td>
     </tr>
+    <tr>
+      <td>200170116014</td>
+      <td>Foram</td>
+      <td><a href="cv.php">View</a></td>
+    </tr>
+    <tr>
+      <td>200170116035</td>
+      <td>Foram</td>
+      <td><a href="cv.php">View</a></td>
+    </tr>
+    <tr>
+      <td>200170116076</td>
+      <td>Foram</td>
+      <td><a href="cv.php">View</a></td>
+    </tr>
     </tbody>
     
 </table>
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+</script>
+
+<!-- <script>
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+</script> -->
 
 </body>
 </html>
