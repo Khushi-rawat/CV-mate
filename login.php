665ac431d79ab3ee67c15e9a3a67679270dd9c47
Krupalee Suriya <suriyark3342@gmail.com>
2023-03-31 18:31:51 +0530

    login

--- login.php
+++ login.php
@@ -15,26 +15,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
-<body class="m-5">
-    <form method="post">
-  <div class="m-5">
+<body ><form method="post">
+<section class="vh-100" style="background-color: #003975;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <h3 class="mb-5">Login</h3>
+            <div class="mb-2">
     <select class="form-select" aria-label="Default select example" name="user">
   <option value="student" selected>Student</option>
   <option value="faculty">Faculty</option>
 </select>
   </div>
-  <div class="m-5">
-    <label for="exampleInputEmail1" class="form-label">ID</label>
-    <input type="number" class="form-control" aria-describedby="emailHelp" name="id">
+  <div class="mb-2">
+    <label for="exampleInputEmail1" class="form-label"></label>
+    <input type="number" class="form-control" aria-describedby="emailHelp" placeholder="Id" name="id">
   </div>
-  <div class="m-5">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+  <div class="mb-5">
+    <label for="exampleInputPassword1" class="form-label"></label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
   </div>
   
-  <button type="submit" class="btn btn-primary m-5 mt-1">Submit</button>
-  <div class="m-3">
-    <a href="register.php">register here..</a>
+  <button type="submit" class="btn btn-primary m-5 mt-1" style="background: #003975;">Login</button>
+  <div>
+  <a href="register.php"><button type="button" class="btn btn-primary m-5 mt-1" style="background-color: #003975;">Register here</button></a>
+   
   </div>
 </form>
 <?php
