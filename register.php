8ac2c98f6cb7b549ed6acb872a64f4324357f71a
Krupalee Suriya <suriyark3342@gmail.com>
2023-03-31 18:54:24 +0530

    register

--- register.php
+++ register.php
@@ -16,31 +16,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
-<body class="m-5">
-    <form method="post">
-  <div class="m-5">
+<body ><form method="post">
+<section class="vh-100" style="background-color: #003975;">
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+    
+  <div class="mb-2">
     <select class="form-select" aria-label="Default select example" name="user">
   <option value="student">Student</option>
   <option value="faculty">Faculty</option>
 </select>
   </div>
-  <div class="m-5">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+  <div class="mb-2">
+    <label for="exampleInputEmail1" class="form-label"></label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email address" aria-describedby="emailHelp" name="email">
   </div>
-  <div class="m-5">
-    <label for="exampleInputId1" class="form-label">ID</label>
-    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="id">
+  <div class="mb-2">
+    <label for="exampleInputId1" class="form-label"></label>
+    <input type="number" class="form-control" id="exampleInputEmail1" placeholder="ID/Enrollment"  aria-describedby="emailHelp" name="id">
   </div>
-  <div class="m-5">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+  <div class="mb-2">
+    <label for="exampleInputPassword1" class="form-label"></label>
+    <input type="password" class="form-control" placeholder="Password" id="exampleInputPassword1" name="password">
   </div>
-  <div class="m-5">
-    <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="cpassword">
+  <div class="mb-5">
+    <label for="exampleInputPassword2" class="form-label"></label>
+    <input type="password" class="form-control" placeholder="Confirm Password" id="exampleInputPassword1" name="cpassword">
   </div>
-  <button type="submit" class="btn btn-primary m-5 mt-1" name="submit">Submit</button>
+  <button type="submit" class="btn btn-primary" style="background-color: #003975;" name="submit">Register</button>
+      </div>
 </form>
 <?php include 'validate.php';?>
 </body>
